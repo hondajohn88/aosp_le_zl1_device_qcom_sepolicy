77eb30c723b18b38b16af6b76df2111fe147f8c3
hondajohn88 <hondajohn88@gmail.com>
2017-01-05 16:02:37 -0800

    Commit for Frankenstein AOSP 7.1.1

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,9 +1,6 @@
 # Board specific SELinux policy variable definitions
-BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
+BOARD_SEPOLICY_DIRS += \
        device/qcom/sepolicy \
        device/qcom/sepolicy/common \
        device/qcom/sepolicy/test \
-       device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
-
--include vendor/du/sepolicy/qcom/sepolicy.mk
+       device/qcom/sepolicy/msm8996
